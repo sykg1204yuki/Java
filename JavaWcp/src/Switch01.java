adca4127a6ea612e743fce33e21f824091613f8e
sykg1204yuki <y.yuki040507@gmail.com>
2021-09-23 19:43:27 +0900

    Java7章

---
+++ JavaWcp/src/Switch01.java
@@ -0,0 +1,16 @@
+
+public class Switch01 {
+	public static void main(String[] args) {
+		int val = 2;
+		switch (val) {
+		case 3:
+			System.out.println("よくできました！");
+			break;
+		case 2:
+			System.out.println("普通でした！");
+			break;
+		case 1:
+			System.out.println("頑張ろう！");
+		}
+	}
+}
