7118216c0e0b9fad4cff67eb2f582bf193d0c4e8
sykg1204yuki <y.yuki040507@gmail.com>
2021-10-08 15:13:44 +0900

    Java12章

---
+++ JavaWcp/src/Capsule/HumanCapsule/HumanNoCapsule.java
@@ -0,0 +1,12 @@
+package Capsule.HumanCapsule;
+
+public class HumanNoCapsule {
+	public String name;
+	public int age;
+	
+	public HumanNoCapsule(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+
+}
---
+++ JavaWcp/src/Chapter/man/CapsuleMan.java
@@ -0,0 +1,24 @@
+package Chapter.man;
+
+public class CapsuleMan {
+	private String name;
+	private int age;
+	
+	public CapsuleMan(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public int getAge() {
+		return this.age;
+	}
+	
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+}
