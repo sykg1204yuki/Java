ee0645c0c9578dcadae65707804231a770648799
sykg1204yuki <y.yuki040507@gmail.com>
2021-10-25 15:19:16 +0900

    Progate演習

--- JavaWcp/src/Progate/Person.java
+++ JavaWcp/src/Progate/Person.java
@@ -1,7 +1,8 @@
 package Progate;
 
-public class Person {
-	public static void printData(String firstName, String lastName, int age, double height, double weight){
+class Person{
+	  public static void printData(String firstName, String lastName, int age, double height, double weight){
+	    
 	    System.out.println("名前は" + fullName(firstName, lastName) + "です");
 	    System.out.println("年齢は" + age + "歳です");
 	    if (age >= 20){
@@ -9,10 +10,17 @@ public class Person {
 	    }else{
 	      System.out.println("未成年者です");
 	    }
-	    System.out.println("身長は" + height + "mです");
+	    System.out.println("身長は" + height + "ｍです");
 	    System.out.println("体重は" + weight + "kgです");
 	    double bmi = bmi(height, weight);
 	    System.out.println("BMIは" + Math.round(bmi) + "です");
+	    if (isHealthy(bmi)){
+	      System.out.println("健康です");
+	    }else{
+	      System.out.println("健康ではありません");
+	    }
+	    
+	    
 	    
 	  }
 	  
@@ -23,4 +31,13 @@ public class Person {
 	  public static double bmi(double height, double weight){
 	    return weight / height / height;
 	  }
-}
+	  
+	  public static boolean isHealthy(double bmi){
+	    return bmi >= 18.5 && bmi < 25.0;
+	  }
+	  
+	  public static double averageAge(int totalAge, int count){
+	    return totalAge/count;
+	  }
+	}
+
