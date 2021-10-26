9d3641182335937c83514a2c94a2cd1b4a2b2812
sykg1204yuki <y.yuki040507@gmail.com>
2021-10-26 15:04:31 +0900

    Progate演習

---
+++ JavaWcp/src/Progate/Main02.java
@@ -0,0 +1,44 @@
+package Progate;
+
+import java.util.Scanner;
+
+class Main02 {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("何人分の情報を入力しますか：");
+    int count = scanner.nextInt();
+    int maxAge = 0;
+    int totalAge = 0;
+    
+    
+    for (int i = 0; i < count; i++){
+      System.out.println(i+1 + "人目");
+      
+      System.out.print("名前：");
+      String firstName = scanner.next();
+      
+      System.out.print("名字：");
+      String lastName = scanner.next();
+      
+      System.out.print("年齢：");
+      int age = scanner.nextInt();
+      if (age > maxAge){
+        maxAge = age;
+      }
+      totalAge += age;
+      
+      System.out.print("身長(m)：");
+      double height = scanner.nextDouble();
+      
+      System.out.print("体重(kg)：");
+      double weight = scanner.nextDouble();
+      
+      Person02.printData(firstName, lastName, age, height, weight);
+    }
+    System.out.println("最高年齢は" + maxAge + "歳です");
+    
+    System.out.println("平均年齢は" + Person02.averageAge(totalAge, count) + "歳です");
+  }
+  
+  
+}
